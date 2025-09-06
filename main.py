7e75e580c46dc87a006f5ea8e5c5cbca309b36e5
juliamorandini <j.morandini@alumno.um.edu.ar>
2025-09-05 21:19:04 -0300

    fallaba todo, ayuda

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def suma (a,b):
+def suma(a, b):
     return a + b
 
-if __name__ == '__main__':
-    print (suma(2,3))
+def resta(a, b):
+    return a - b
--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import unittest
+
 from main import suma
 
-class TestSuma(unittest.TestCase):
+class TestPrueba(unittest.TestCase):
+
     def test_suma(self):
         self.assertEqual(suma(2, 3), 5)
 
+    def test_resta(self):
+        self.assertEqual(suma(5, -3), 2)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    unittest.main()
